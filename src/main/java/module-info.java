7d526b135c0ea3514b2e32628cad97fb44727b87
Luke Edward Schofield <schofieldluke14@gmail.com>
2026-05-19 11:37:29 +0800

    (build): *changed project structure to mvc *configured pom.xml to include dependencies *included mysql as a dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,20 @@
-module com.example.oopsystem {
+module oopsystem {
+//    javaFX modules
     requires javafx.controls;
     requires javafx.fxml;
 
+//    MySQL JDBC
+    requires java.sql;
+
+//    Extra UI libraries
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.oopsystem to javafx.fxml;
-    exports com.example.oopsystem;
+//    Open packages to JavaFX for reflection
+    opens oopsystem.app to javafx.graphics;
+
+
+//    Export entry point
+    exports oopsystem.app;
 }
--- src/main/java/oopsystem/app/Main.java
+++ src/main/java/oopsystem/app/Main.java
@@ -1,4 +1,5 @@
-package com.example.oopsystem;
+package oopsystem.app;
+
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,13 +8,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/oopsystem/view/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
