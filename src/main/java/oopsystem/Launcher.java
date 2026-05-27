7207663193406d0c6a92e24be75a93ecb682a414
Luke Edward Schofield <schofieldluke14@gmail.com>
2026-05-27 16:11:41 +0800

    chore: remove accidentally committed .env file

--- src/main/java/oopsystem/Launcher.java
+++ src/main/java/oopsystem/Launcher.java
@@ -1,4 +1,14 @@
 package oopsystem;
 
+
+import javafx.application.Application;
+import oopsystem.app.EmployeeApplication;
+
+
 public class Launcher {
+
+    public static void main(String[] args){
+        Application.launch(EmployeeApplication.class, args);
+    }
+
 }
--- src/main/java/oopsystem/app/EmployeeApplication.java
+++ src/main/java/oopsystem/app/EmployeeApplication.java
@@ -1,16 +1,17 @@
 package oopsystem.app;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+public class EmployeeApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/oopsystem/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(EmployeeApplication.class.getResource("/oopsystem/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
