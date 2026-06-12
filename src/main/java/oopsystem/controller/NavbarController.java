1901e34a67f143cbdcad208e17a2cc3e44f42677
Luke Edward Schofield <schofieldluke14@gmail.com>
2026-06-13 01:16:17 +0800

    feat: integrated the navigation between modules

--- src/main/java/oopsystem/controller/NavbarController.java
+++ src/main/java/oopsystem/controller/NavbarController.java
@@ -12,9 +12,9 @@ public class NavbarController {
     public Button employeeDirectoryMenu;
     public Button reportsMenu;
 
-    //    @FXML public void goToDashboard(){SceneNavigator.switchTo("dashboard/ashboardView");}
+    //    @FXML public void goToDashboard(){SceneNavigator.switchTo("dashboard/dashboardView");}
     @FXML public void goToPassSlipIssuance(){SceneNavigator.switchTo("passSlipIssuance/PassSlipIssuanceView");}
-    //    @FXML public void goToMovementLogs(){SceneNavigator.switchTo("movementLogs/MovementLogsView");}
+    @FXML public void goToMovementLogs(){SceneNavigator.switchTo("movementLogs/MovementLogsView");}
     @FXML public void goToEmployeeDirectory(){SceneNavigator.switchTo("employeeDirectory/EmployeeDirectoryView");}
     @FXML public void gotoReports(){SceneNavigator.switchTo("reports/ReportsView");}
     @FXML public void goToProfile(){SceneNavigator.switchTo("profile/ProfileView");}
